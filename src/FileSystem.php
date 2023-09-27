575d40c026fd84832de2d57b332ff566d1516fbb
YangHongwei <403236160@qq.com>
2023-09-27 18:31:57 +0800

    feat:增加文件类的删除功能

---
+++ src/FileSystem.php
@@ -0,0 +1,52 @@
+<?php
+// +----------------------------------------------------------------------
+// | NewThink [ Think More,Think Better! ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016~2030 http://www.sxqibo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | 版权所有：山西岐伯信息科技有限公司
+// +----------------------------------------------------------------------
+// | Author: yanghongwei  Date:2023/9/27 Time:18:09
+// +----------------------------------------------------------------------
+
+namespace Sxqibo\FastUtils;
+
+/**
+ * 访问和操作文件系统
+ */
+class FileSystem
+{
+    /**
+     * 递归删除目录
+     */
+    public static function delDir(string $dir, bool $delSelf = true): bool
+    {
+        // 检查目录是否存在
+        if (!is_dir($dir)) {
+            return false;
+        }
+
+        // 迭代目录中的文件和子目录
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        // 遍历$files中的每个文件和子目录
+        foreach ($files as $fileInfo) {
+            if ($fileInfo->isDir()) {
+                // 删除该目录及其内容
+                self::delDir($fileInfo->getRealPath());
+            } else {
+                // 如果不是目录，就使用@unlink函数来删除文件
+                @unlink($fileInfo->getRealPath());
+            }
+        }
+
+        if ($delSelf) {
+            @rmdir($dir);
+        }
+
+        return true;
+    }
+}
---
+++ tests/TestFilesystem.php
@@ -0,0 +1,43 @@
+<?php
+// +----------------------------------------------------------------------
+// | NewThink [ Think More,Think Better! ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016~2030 http://www.sxqibo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | 版权所有：山西岐伯信息科技有限公司
+// +----------------------------------------------------------------------
+// | Author: yanghongwei  Date:2023/9/27 Time:18:21
+// +----------------------------------------------------------------------
+
+use PHPUnit\Framework\TestCase;
+use Sxqibo\FastUtils\FileSystem;
+
+class TestFilesystem  extends TestCase
+{
+
+    public function testOne()
+    {
+        // test目录
+        $directoryToDelete = dirname(__FILE__).'/../public/runtime/test';
+
+        // 调用 delDir 方法，将 $delSelf 参数设为 true，表示删除目录本身
+        if (Filesystem::delDir($directoryToDelete, true)) {
+            echo "目录删除成功！";
+        } else {
+            echo "无法删除目录。";
+        }
+    }
+
+    public function testTwo()
+    {
+        // test目录
+        $directoryToDelete = dirname(__FILE__).'/../public/runtime/test';
+
+        // 调用 delDir 方法，将 $delSelf 参数设为 true，表示删除目录本身
+        if (Filesystem::delDir($directoryToDelete, false)) {
+            echo "目录删除成功！";
+        } else {
+            echo "无法删除目录。";
+        }
+    }
+}
